b490dbb35b84c4eb2a83988bf72f5602e18cb8be
Kepsert <voskasper@gmail.com>
2024-12-11 01:01:12 +0100

    Update SpeedSorting.cs

--- SpeedSorting.cs
+++ SpeedSorting.cs
@@ -2,11 +2,15 @@
 {
     internal class SpeedSorting
     {
+        ColorRandomizer _colorRandomizer;
+        
         /// <summary>
         /// Bubble sorts a list of doubles to find the fastest speed
         /// </summary>
         public static void SortBySpeed()
         {
+            _colorRandomizer = new ColorRandomizer();
+            
             Console.ForegroundColor = ConsoleColor.Cyan;
             List<double> speeds = new List<double> {12, 123, 54, 34, 6, 70, 57, 120, 634, 420 };
             double temp = 0;
@@ -29,9 +33,9 @@
 
             foreach(double i in speeds)
             {
+                Console.ForegroundColor = ColorRandomizer.GetRandomForegroundColor(ColorRandomizer.GetRandomColor());
                 Console.Write($"{i} ");
             }
-            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
